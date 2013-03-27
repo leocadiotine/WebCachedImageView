52b3a14e7123053b8bb7eb9fd2520a92b0c668a5
Leocadio Tiné <leocadiotine@gmail.com>
2013-03-26 20:33:13 -0400

    Modifies the project's logic. Instead of a helper class, it is now a cached ImageView and can be accessed directly via XML. Now the project is called WebCachedImageView.

---
+++ code/src/io/leocad/webcachedimageview/CacheManager.java
@@ -0,0 +1,126 @@
+package io.leocad.webcachedimageview;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.os.Environment;
+import android.support.v4.util.LruCache;
+
+
+public class CacheManager {
+
+	static final int MODE_NO_CACHE = 0;
+	static final int MODE_MEMORY = 1;
+	static final int MODE_DISK = 2;
+
+	private static CacheManager INSTANCE = null;
+
+	public static CacheManager getInstance(Context context, int mode, float memoryFractionToUse) {
+
+		if (INSTANCE == null || INSTANCE.mMode != mode || INSTANCE.mMemoryFractionToUse != memoryFractionToUse) {
+			INSTANCE = new CacheManager(context, mode, memoryFractionToUse);
+		}
+
+		return INSTANCE;
+	}
+
+	private int mMode;
+	private float mMemoryFractionToUse;
+	private LruCache<String, Bitmap> mMemoryCache;
+	private File mCacheDir;
+
+	private CacheManager(Context context, int mode, float memoryFractionToUse) {
+
+		if ((mode & MODE_MEMORY) == MODE_MEMORY) {
+
+			if (memoryFractionToUse >= 1.f) {
+				throw new RuntimeException("CachedImageFetcher can't use more than 99% of the device's memory! Please specify a smaller memoryFractionToUse, like 1/8.");
+			}
+
+			final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+			final int cacheSize = (int) (maxMemory * memoryFractionToUse);
+
+			mMemoryCache = new LruCache<String, Bitmap>(cacheSize) {
+				@Override
+				protected int sizeOf(String key, Bitmap bitmap) {
+					return (bitmap.getRowBytes() * bitmap.getHeight()) / 1024;
+				}
+			};
+		}
+
+		if ((mode & MODE_DISK) == MODE_DISK) {
+			mCacheDir = getCacheDir(context);
+		}
+	}
+
+	@SuppressLint("NewApi")
+	private File getCacheDir(Context context) {
+		String externalStorageState = Environment.getExternalStorageState();
+
+		if (Environment.MEDIA_MOUNTED.equals(externalStorageState)) {
+
+			if (Build.VERSION.SDK_INT > 7) {
+				return context.getExternalCacheDir();
+
+			} else {
+				File externalStorageDir = Environment.getExternalStorageDirectory();
+				String cacheDirPath = String.format("/Android/data/%s/cache/", externalStorageDir.getPath());
+
+				//Create the directory (if it doesn't exist)
+				File cacheDir = new File(cacheDirPath);
+				if (!cacheDir.exists()) {
+					cacheDir.mkdirs();
+				}
+
+				return cacheDir;
+			}
+
+		} else {
+			return context.getCacheDir();
+		}
+	}
+	
+	public Bitmap getCachedOnMemory(String url) {
+		
+		if ((mMode & MODE_MEMORY) == MODE_MEMORY) {
+			return mMemoryCache.get(url);
+		}
+		
+		return null;
+	}
+	
+	public Bitmap getCachedOnDisk(String url) {
+
+		if ((mMode & MODE_DISK) == MODE_DISK) {
+			
+			try {
+				File cachedFile = new File(mCacheDir, getFileName(url));
+				FileInputStream fis = new FileInputStream(cachedFile);
+				fis.close();
+				
+				return null; //TODO
+				
+			} catch (Exception e) {
+				// No cached version or error reading file system
+				return null;
+			}
+		}
+		
+		return null;
+	}
+	
+	private static String getFileName(String url) {
+		return String.valueOf( url.hashCode() );
+	}
+
+	public void cacheonMemory(String url, Bitmap bitmap) {
+
+		if ((mMode & MODE_MEMORY) == MODE_MEMORY) {
+			mMemoryCache.put(url, bitmap);
+		}
+	}
+}
---
+++ code/src/io/leocad/webcachedimageview/WebCachedImageView.java
@@ -0,0 +1,104 @@
+package io.leocad.webcachedimageview;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.ImageView;
+
+
+public class WebCachedImageView extends ImageView {
+
+	private CacheManager mCacheMgr;
+
+	public WebCachedImageView(Context context) {
+		super(context);
+		init(context, CacheManager.MODE_MEMORY | CacheManager.MODE_DISK, 1/8);
+	}
+	
+
+	public WebCachedImageView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		
+		TypedArray styledAttrs = context.obtainStyledAttributes(attrs, R.styleable.WebCachedImageView);
+		int mode = styledAttrs.getInt(R.styleable.WebCachedImageView_mode, CacheManager.MODE_MEMORY | CacheManager.MODE_DISK);
+		float memoryFractionToUse = styledAttrs.getFraction(R.styleable.WebCachedImageView_memoryPercentToUse, 1, 1, 1/8);
+		styledAttrs.recycle();
+		
+		init(context, mode, memoryFractionToUse);
+	}
+
+	private void init(Context context, int mode, float memoryFractionToUse) {
+
+		mCacheMgr = CacheManager.getInstance(context, mode, memoryFractionToUse);
+	}
+
+	public void setImageUrl(String url) {
+
+		setImageBitmap(null); // TODO Add placeholder
+
+		new AsyncTask<String, Void, Bitmap>() {
+			
+			@Override
+			protected Bitmap doInBackground(String... params) {
+				
+				String url = params[0];
+				Bitmap bitmap = null;
+				
+				// Check for cached versions
+				if (
+						// First, check in memory
+						((bitmap = mCacheMgr.getCachedOnMemory(url)) != null)
+						// Then, check in disk
+						|| ((bitmap = mCacheMgr.getCachedOnDisk(url)) != null)
+						
+					) {
+					return bitmap;
+				}
+				
+				// No cached versions. Download it
+				try {
+					InputStream is = fetchStream(url);
+					
+					is.close();
+					
+					
+				} catch (IOException e) {
+					Log.e("CachedImageFetcher", "Can't download image at " + url, e);
+				}
+				
+				// And cache it
+				if (bitmap != null) {
+					mCacheMgr.cacheonMemory(url, bitmap);
+
+					// TODO Cache in disk
+				}
+				
+				return null;
+			}
+			
+			@Override
+			protected void onPostExecute(Bitmap bitmap) {
+			
+				setImageBitmap(bitmap);
+			}
+		}.execute(url);
+	}
+
+	private static InputStream fetchStream(String urlString) throws IllegalStateException, IOException {
+
+		DefaultHttpClient httpClient = new DefaultHttpClient();
+		HttpGet request = new HttpGet(urlString);
+		HttpResponse response = httpClient.execute(request);
+		return response.getEntity().getContent();
+	}
+}
