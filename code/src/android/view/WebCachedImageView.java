82096fbd6339175cc294d933ff200d35778044f8
Leocadio Tiné <leocadiotine@gmail.com>
2013-03-27 23:35:01 -0400

    Extracts download task to BtimapWorkerTask.

--- code/src/android/view/WebCachedImageView.java
+++ code/src/android/view/WebCachedImageView.java
@@ -1,29 +1,18 @@
 package android.view;
 
 
+import io.leocad.webcachedimageview.BitmapWorkerTask;
 import io.leocad.webcachedimageview.CacheManager;
 import io.leocad.webcachedimageview.R;
-
-import java.io.IOException;
-import java.io.InputStream;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.widget.ImageView;
 
 
 public class WebCachedImageView extends ImageView {
 
-	private CacheManager mCacheMgr;
+	public CacheManager mCacheMgr;
 
 	public WebCachedImageView(Context context) {
 		super(context);
@@ -51,58 +40,7 @@ public class WebCachedImageView extends ImageView {
 	public void setImageUrl(String url) {
 
 		setImageBitmap(null); // TODO Add placeholder
-
-		new AsyncTask<String, Void, Bitmap>() {
-			
-			@Override
-			protected Bitmap doInBackground(String... params) {
-				
-				String url = params[0];
-				Bitmap bitmap = null;
-				
-				// Check for cached versions
-				if (
-						// First, check in memory
-						((bitmap = mCacheMgr.getCachedOnMemory(url)) != null)
-						// Then, check in disk
-						|| ((bitmap = mCacheMgr.getCachedOnDisk(url)) != null)
-						
-					) {
-					return bitmap;
-				}
-				
-				// No cached versions. Download it
-				try {
-					InputStream is = fetchStream(url);
-					bitmap = BitmapFactory.decodeStream(is); // TODO Add sampling
-					is.close();
-					
-				} catch (IOException e) {
-					Log.e("CachedImageFetcher", "Can't download image at " + url, e);
-				}
-				
-				// And cache it
-				if (bitmap != null) {
-					mCacheMgr.cacheOnMemory(url, bitmap);
-					mCacheMgr.cacheOnDisk(url, bitmap);
-				}
-				
-				return bitmap;
-			}
-			
-			@Override
-			protected void onPostExecute(Bitmap bitmap) {
-			
-				setImageBitmap(bitmap);
-			}
-		}.execute(url);
-	}
-
-	private static InputStream fetchStream(String urlString) throws IllegalStateException, IOException {
-
-		DefaultHttpClient httpClient = new DefaultHttpClient();
-		HttpGet request = new HttpGet(urlString);
-		HttpResponse response = httpClient.execute(request);
-		return response.getEntity().getContent();
+		
+		new BitmapWorkerTask(this).execute(url, mCacheMgr);
 	}
 }
---
+++ code/src/io/leocad/webcachedimageview/BitmapWorkerTask.java
@@ -0,0 +1,82 @@
+package io.leocad.webcachedimageview;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.ref.WeakReference;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.WebCachedImageView;
+
+
+public class BitmapWorkerTask extends AsyncTask<Object, Void, Bitmap> {
+
+	private WeakReference<WebCachedImageView> mImageViewReference;
+	private String mUrl;
+
+	public BitmapWorkerTask(WebCachedImageView iv) {
+		mImageViewReference = new WeakReference<WebCachedImageView>(iv);
+	}
+
+	@Override
+	protected Bitmap doInBackground(Object... params) {
+
+		mUrl = (String) params[0];
+		CacheManager cacheMgr = (CacheManager) params[1];
+		Bitmap bitmap = null;
+
+		// Check for cached versions
+		if (
+				// First, check in memory
+				((bitmap = cacheMgr.getCachedOnMemory(mUrl)) != null)
+				// Then, check in disk
+				|| ((bitmap = cacheMgr.getCachedOnDisk(mUrl)) != null)
+
+			) {
+			return bitmap;
+		}
+
+		// No cached versions. Download it
+		try {
+			InputStream is = fetchStream(mUrl);
+			bitmap = BitmapFactory.decodeStream(is); // TODO Add sampling
+			is.close();
+
+		} catch (IOException e) {
+			Log.e("WebCachedImageView", "Can't download image at " + mUrl, e);
+		}
+
+		// And cache it
+		if (bitmap != null) {
+			cacheMgr.cacheOnMemory(mUrl, bitmap);
+			cacheMgr.cacheOnDisk(mUrl, bitmap);
+		}
+
+		return bitmap;
+	}
+
+	@Override
+	protected void onPostExecute(Bitmap bitmap) {
+		
+		if (mImageViewReference != null && bitmap != null) {
+            final WebCachedImageView imageView = mImageViewReference.get();
+            if (imageView != null) {
+                imageView.setImageBitmap(bitmap);
+            }
+        }
+	}
+
+	private static InputStream fetchStream(String urlString) throws IllegalStateException, IOException {
+
+		DefaultHttpClient httpClient = new DefaultHttpClient();
+		HttpGet request = new HttpGet(urlString);
+		HttpResponse response = httpClient.execute(request);
+		return response.getEntity().getContent();
+	}
+}
